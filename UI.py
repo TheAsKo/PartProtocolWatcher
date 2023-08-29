fd5569e51edf8f8c3ac5eda9a825759f9ae9e38d
TheAsKo <107570375+TheAsKo@users.noreply.github.com>
2023-08-29 03:56:56 +0200

    Preparing dev2 v1

--- UI.py
+++ UI.py
@@ -33,16 +33,14 @@ def center(win):
 def ButtonPress(arg):
     logging.debug("Assigning ID: "+str(arg))
     match arg:
-        case 1 : Config.ConfigWrite('FileWatcher','watcheddirectory',Config.ConfigRead('FileWatcher','DM1Directory','str'),'str')
-        case 2 : Config.ConfigWrite('FileWatcher','watcheddirectory',Config.ConfigRead('FileWatcher','DM2Directory','str'),'str')
-        case 3 : Config.ConfigWrite('FileWatcher','watcheddirectory',Config.ConfigRead('FileWatcher','EMDirectory','str'),'str')
-        case 4 : Config.ConfigWrite('FileWatcher','watcheddirectory',Config.ConfigRead('FileWatcher','TestDirectory','str'),'str')
-        case 0 : pass #NEED TO FINISH AUTODETECT FUNC
+        case 1 : pass #Run into Main Cycle 
+        case 2 : pass # UI - Toplevel directory adding
+        case 3 : pass # Force Quit
         case _ : logging.critical("Failed passing command from UI")
-    Lib.FileWatcher.WatchDirectory = Config.ConfigRead('FileWatcher','WatchedDirectory','str')
+    # This is unused as of rn --- Lib.FileWatcher.WatchDirectory = Config.ConfigRead('FileWatcher','WatchedDirectory','str')
     root.lift()
     root.destroy()
-    Lib.FileWatcher().run()
+    # This needs to go out cuz this one start process --- Lib.FileWatcher().run()
 ##############################################################################################    
 
     
@@ -52,22 +50,21 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-ttk.Button(mainframe, text="DM1",command=lambda:ButtonPress(1)).grid(column=1, row=3, sticky=W)
-ttk.Button(mainframe, text="DM2",command=lambda:ButtonPress(2)).grid(column=2, row=3, sticky=W)
-ttk.Button(mainframe, text="EM",command=lambda:ButtonPress(3)).grid(column=3, row=3, sticky=W)
-ttk.Button(mainframe, text="AutoDetect",command=lambda:ButtonPress(0),state=DISABLED).grid(column=2, row=4, sticky=W)
-ttk.Button(mainframe, text="Test",command=lambda:ButtonPress(4)).grid(column=2, row=5, sticky=W)
+ttk.Button(mainframe, text="Start",command=lambda:ButtonPress(1)).grid(column=1, row=3, sticky=W)
+ttk.Button(mainframe, text="Spravovanie protokolov",command=lambda:ButtonPress(2)).grid(column=2, row=3, sticky=W)
+ttk.Button(mainframe, text="Vypnut",command=lambda:ButtonPress(3)).grid(column=3, row=3, sticky=W)
+#ttk.Button(mainframe, text="AutoDetect",command=lambda:ButtonPress(0),state=DISABLED).grid(column=2, row=4, sticky=W)
+#ttk.Button(mainframe, text="Test",command=lambda:ButtonPress(4)).grid(column=2, row=5, sticky=W)
 
 
-ttk.Label(mainframe, text="13376_400").grid(column=1, row=2, sticky=N)
-ttk.Label(mainframe, text="13376_401").grid(column=2, row=2, sticky=N)
-ttk.Label(mainframe, text="13114_402").grid(column=3, row=2, sticky=N)
+#ttk.Label(mainframe, text="13376_400").grid(column=1, row=2, sticky=N)
+#ttk.Label(mainframe, text="13376_401").grid(column=2, row=2, sticky=N)
+#ttk.Label(mainframe, text="13114_402").grid(column=3, row=2, sticky=N)
 
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
 
 center(root)
-
 
 if __name__ == '__main__':
     logging.debug("Root Start")
